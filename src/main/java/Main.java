7946cebcb808a916beb58a555904ed2e8f1411b0
Joonsung Gil <sa82trip@gmail.com>
2022-09-03 11:49:52 +0900

    Sto0002: create User and UserDao
    before: initial commit
    
    now: can add user and retrieve user via DB

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException { UserDao dao = new UserDao();
+        User user = new User(); user.setId("ordinaryDev"); user.setName("J"); user.setPassword("unSaltedPassword");
+        dao.add(user);
+        System.out.println(user.getId() + " 등록 성공");
+        User user2 = dao.get(user.getId()); System.out.println(user2.getName()); System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
