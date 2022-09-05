147124d926a00d4d35ba0f8aac78d7fd0b78b21a
Joonsung Gil <sa82trip@gmail.com>
2022-09-06 08:44:59 +0900

    Sto0003: make connection more loose
    before: UserDaoTest made ConnectionMaker
    
    now: DaoFactory takes place. now DaoFactory is charge of creating objects and make relationship

---
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -0,0 +1,10 @@
+package springbook.user.dao;
+
+
+public class DaoFactory {
+    public UserDao userDao () {
+       ConnectionMaker connectionMaker = new DConnectionMaker();
+        return new UserDao(connectionMaker);
+    }
+
+}
--- src/main/test/springbook/user/dao/UserDaoTest.java
+++ src/main/test/springbook/user/dao/UserDaoTest.java
@@ -11,13 +11,7 @@ public class UserDaoTest {
     @Test
     public void add() throws SQLException, ClassNotFoundException {
 
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-
-        /* client가 의존관계를 만들어 주는 부분 */
-        UserDao dao = new UserDao(connectionMaker);
-
-        /* 만약 다른 connectionMaker를 만든다면 UserDao의 코드는 만질 필요 없이
-        * DB와 연결 하는 기능을 사용할 수 있게 되었다. */
+        UserDao dao = new DaoFactory().userDao();
 
         dao.deleteAll();
         System.out.println("Table 초기화 완료!");
