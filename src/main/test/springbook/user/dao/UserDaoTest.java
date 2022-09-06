be63b15982117b1eb41cb485025ee40a65d37655
Joonsung Gil <sa82trip@gmail.com>
2022-09-06 21:24:20 +0900

    Sto0003: apply springFramework to use IoC
    before: just use DaoFactory to create Object and make relationship
    
    now: use some annotation to make applicationContext and beans. now I am able to get UserDao by using applicationContext
    
    to use spring, added seven jar file because it seems mvn repo does not hold these jars anymore.

--- src/main/test/springbook/user/dao/UserDaoTest.java
+++ src/main/test/springbook/user/dao/UserDaoTest.java
@@ -1,25 +1,30 @@
 package springbook.user.dao;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
 
 public class UserDaoTest {
+   public static void main (String[] args) throws SQLException, ClassNotFoundException {
+
+       ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+       UserDao dao = context.getBean("userDao", UserDao.class);
+
+       dao.deleteAll();
+       System.out.println("Table 초기화 완료!");
+       User user = new User(); user.setId("ordinaryD"); user.setName("J"); user.setPassword("password");
+       dao.add(user);
+       System.out.println(user.getId() + " 등록 성공");
+       User user2 = dao.get(user.getId()); System.out.println(user2.getName()); System.out.println(user2.getPassword());
+       System.out.println(user2.getId() + " 조회 성공");
+   }
 
     @Test
-    public void add() throws SQLException, ClassNotFoundException {
-
-        UserDao dao = new DaoFactory().userDao();
-
-        dao.deleteAll();
-        System.out.println("Table 초기화 완료!");
-        User user = new User(); user.setId("ordinaryD"); user.setName("J"); user.setPassword("password");
-        dao.add(user);
-        System.out.println(user.getId() + " 등록 성공");
-        User user2 = dao.get(user.getId()); System.out.println(user2.getName()); System.out.println(user2.getPassword());
-        System.out.println(user2.getId() + " 조회 성공");
+    public void add() {
     }
 
     @Test
