580b672d0c3b1981f591b667110310720777e554
Joonsung Gil <sa82trip@gmail.com>
2022-09-11 17:14:54 +0900

    Improvement of unit test
    1. automotive assertion
    
    note: now do not have to compare the values by my eyes.

--- src/main/test/springbook/user/dao/CountingUserDaoTest.java
+++ src/main/test/springbook/user/dao/CountingUserDaoTest.java
@@ -15,11 +15,24 @@ public class CountingUserDaoTest {
 
        dao.deleteAll();
        System.out.println("Table 초기화 완료!");
-       User user = new User(); user.setId("ordinaryD"); user.setName("J"); user.setPassword("password");
+       User user = new User();
+       user.setId("ordinaryD");
+       user.setName("J");
+       user.setPassword("password");
        dao.add(user);
        System.out.println(user.getId() + " 등록 성공");
-       User user2 = dao.get(user.getId()); System.out.println(user2.getName()); System.out.println(user2.getPassword());
-       System.out.println(user2.getId() + " 조회 성공");
+
+       User user2 = dao.get(user.getId());
+       System.out.println(user2.getName());
+       System.out.println(user2.getPassword());
+
+       if(!user.getName().equals(user2.getName())){
+           System.out.printf("Test failed on comparing Name expected - %s : actual - %s %n",user.getName(), user2.getName());
+       }
+       else if(!user.getPassword().equals(user2.getPassword())){
+           System.out.printf("Test filed on comparing Password expected - %s : actual - %s %n",user.getPassword(), user2.getPassword());
+       }
+       else System.out.println(user2.getId() + " 조회 test 성공");
        CountingConnectionMaker ccm = context.getBean("getConnectionMaker", CountingConnectionMaker.class);
        System.out.printf("UserDao was used %d times%n",ccm.getCounter());
    }
