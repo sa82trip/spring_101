1be1f8c19c3f8ab144c452ab76b800a9ded39e02
Joonsung Gil <sa82trip@gmail.com>
2022-09-11 23:47:33 +0900

    Sto0003: JUnit test with JUnitCore.main()
    This method allows me to select a class file to run tests which the class file includes. However, this is less convenient than using IDE's help

---
+++ src/main/test/springbook/TestMain.java
@@ -0,0 +1,10 @@
+package springbook;
+
+import org.junit.runner.JUnitCore;
+
+public class TestMain {
+
+   public static void main(String[] args) {
+       JUnitCore.main("springbook.user.dao.UserDaoTest");
+   }
+}
