bb44a694338cc4cb9909407be8496ad829896559
Joonsung Gil <sa82trip@gmail.com>
2022-09-12 09:58:10 +0900

    Sto0002: remove Stdout on DConnectionMaker

--- src/main/java/springbook/user/dao/DConnectionMaker.java
+++ src/main/java/springbook/user/dao/DConnectionMaker.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 public class DConnectionMaker implements ConnectionMaker {
     @Override
     public Connection makeConnection() throws ClassNotFoundException, SQLException {
-        System.out.println("This is DConnectionMaker which is providing connection");
         Class.forName("com.mysql.cj.jdbc.Driver");  // this part is not necessary anymore
         return DriverManager.getConnection(
                 "jdbc:mysql://localhost:53306/springbook", "spring", "book");
